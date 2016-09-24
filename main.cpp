b49fdd95ca715f2b9541ff011bf8d7beba0c413f
anhnv1sec <anhnvpentest@gmail.com>
2016-09-24 13:38:30 +0700

    commit thu 1 cai .cpp xem nao?

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+
+#include "controller.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Anh Nguyen Viet - 1st Week Pratices\n";
+	cout << "-----------------------------------\n\n";
+
+	menu();
+
+	getch();
+	return 0;
+}
